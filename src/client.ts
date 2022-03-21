e62da49b36ba898edfa49d4e9c480a3d4d619444
dasoncheng <dasoncheng@outlook.com>
2022-03-21 04:48:51 +0000

    feat: rename

--- src/client.ts
+++ src/client.ts
@@ -1,16 +1,16 @@
 import { writeFile } from "fs/promises";
 import { credentials } from "@grpc/grpc-js";
 import { Html2PdfRequest } from "../packages/grpc/grpc-chromium_pb";
-import { GrpcPlaywrightClient } from "../packages/grpc/grpc-chromium_grpc_pb";
+import { GrpcChromiumClient } from "../packages/grpc/grpc-chromium_grpc_pb";
 
 async function application() {
   await Promise.resolve();
-  const client = new GrpcPlaywrightClient("localhost:3000", credentials.createInsecure());
+  const client = new GrpcChromiumClient("localhost:3000", credentials.createInsecure());
   const request = new Html2PdfRequest();
   request.setUrl("https://page.xinluomed.com/test/afc/#/quality-control");
   client.html2Pdf(request, (err, res) => {
     const buf = res.getPdf_asU8();
-    writeFile("abc.pdf", buf);
+    writeFile(`docs/D${Date.now()}.pdf`, buf);
   });
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { Server, ServerCredentials } from "@grpc/grpc-js";
-import services from "../packages/grpc/grpc-chromium_grpc_pb";
+import { GrpcChromiumService } from "../packages/grpc/grpc-chromium_grpc_pb";
 import { html2Pdf } from "./services/grpc-chromium";
 
 async function application() {
   await Promise.resolve();
   const server = new Server();
-  server.addService(services.GrpcPlaywrightService, {
+  server.addService(GrpcChromiumService, {
     html2Pdf,
   });
   server.bindAsync("0.0.0.0:3000", ServerCredentials.createInsecure(), (err, port) => {
